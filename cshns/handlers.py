df9a236f8a60defc3f1381a6f4adba3cc2881a3b
Ryan Brown <rbrown@newstex.com>
2011-08-03 21:22:10 -0700

    Finished SimpleClient, made some progress on server administration

--- cshns/handlers.py
+++ cshns/handlers.py
@@ -5,19 +5,24 @@
 import cshns
 import base64
 import logging
+from cshns.auth import DummyAuthorizer
 from botoweb.exceptions import Unauthorized
 from botoweb.appserver.handlers import RequestHandler
 
-log = logging.getLogger("CSHNS-Server")
+log = logging.getLogger("CSHNS")
+
 
 class CSHNSServer(RequestHandler):
 	"""
 	The server interface, for submitting messages to and administering queues,
 	as well as getting information about available queues
 	"""
+	allowed_methods = ['head', 'get', 'put', 'post', 'options']
 	def __init__(cls, env, config):
 		RequestHandler.__init__(cls, env, config)
+		cls.log = logging.getLogger("CSHNS-Server")
 		cls.queue = None
+		cls.auth = DummyAuthorizer()
 
 	def __call__(cls, request, response, obj_id=None):
 		"""Override this to require an authentication token
@@ -46,40 +51,40 @@ class CSHNSServer(RequestHandler):
 		if auth_token.startswith("HMAC"):
 			uname = base64.decodestring(auth_token.split(' ')[1])
 			passwd = auth_token.split(' ')[2]
-			cls.user = cshns.User.get_by_name(uname)
-			if str(cls.user.password) == passwd:
-				log.info('Authentication successful for %s' % uname)
+			cls.user = cshns.auth.User.get_by_name(uname)
+			if cls.auth.authorize(cls.user, passwd):
+				cls.log.info('Authentication successful for %s' % uname)
 				return True
-			log.info('Authentication failed for %s' % uname)
+			cls.log.info('Authentication failed for %s' % uname)
 			return False
 
 	def _head(cls, request, response, id=None):
 		"""Do shit"""
-		response.status = 202
+		response.status = 200
 		response.body = "OK"
 		return response
 
 	def _get(cls, request, response, id=None):
 		"""Do shit"""
-		response.status = 202
+		response.status = 200
 		response.body = "OK"
 		return response
 
 	def _put(cls, request, response, id=None):
 		"""Do shit"""
-		response.status = 202
+		response.status = 200
 		response.body = "OK"
 		return response
 
 	def _post(cls, request, response, id=None):
 		"""Do shit"""
-		response.status = 202
+		response.status = 200
 		response.body = "OK"
 		return response
 
 	def _options(cls, request, response, id=None):
 		"""Do shit"""
-		response.status = 202
+		response.status = 200
 		response.body = "OK"
 		return response
 
@@ -88,5 +93,36 @@ class SimpleClient(RequestHandler):
 	"""
 	A simple CSHNS client intended for testing and example purposes only
 	"""
+	allowed_methods = ['head', 'get', 'put', 'post']
 	def __init__(cls, env, config):
 		RequestHandler.__init__(cls, env, config)
+		cls.log = logging.getLogger("CSHNS-Client")
+
+	def _head(cls, request, response, id=None):
+		"""Do shit"""
+		cls.log.info("Received HEAD request %s" % request)
+		response.status = 200
+		response.body = "OK"
+		return response
+
+	def _get(cls, request, response, id=None):
+		"""Do shit"""
+		cls.log.info("Received GET request %s" % request)
+		response.status = 200
+		response.body = "OK"
+		return response
+
+	def _put(cls, request, response, id=None):
+		"""Do shit"""
+		cls.log.info("Received PUT request %s" % request)
+		response.status = 200
+		response.body = "OK"
+		return response
+
+	def _post(cls, request, response, id=None):
+		"""Do shit"""
+		cls.log.info("Received POST request %s" % request)
+		response.status = 200
+		response.body = "OK"
+		return response
+
