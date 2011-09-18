37101bbd832da559a2b0b6eea125f91ea67de5a2
Ryan Brown <ryansb@csh.rit.edu>
2011-09-18 02:36:59 -0400

    made username and password required

--- nagini/db/user.py
+++ nagini/db/user.py
@@ -7,7 +7,7 @@ from nagini.db.model import Model
 from nagini.db.property import PasswordProperty
 
 class User(Model):
-	def __init__(self, shortname='', passwd='', longname=None, devices=[]):
+	def __init__(self, shortname, passwd, longname=None, devices=[]):
 		self.name = shortname
 		self.fullname = shortname
 		if longname: self.fullname = longname
